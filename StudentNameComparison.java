a905e7f50f5dd1971ad14091dd03e3c7340b266e
shrawanii15 <shrawanipawar10806@gmail.com>
2026-04-10 19:48:25 +0530

    Add files via upload

---
+++ StudentNameComparison.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class StudentNameComparison {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Accept two student names
+        System.out.print("Enter first student name: ");
+        String name1 = sc.nextLine();
+
+        System.out.print("Enter second student name: ");
+        String name2 = sc.nextLine();
+
+        // Case-sensitive comparison
+        if (name1.equals(name2)) {
+            System.out.println("Case-Sensitive: Names are equal");
+        } else {
+            System.out.println("Case-Sensitive: Names are NOT equal");
+        }
+
+        // Case-insensitive comparison
+        if (name1.equalsIgnoreCase(name2)) {
+            System.out.println("Case-Insensitive: Names are equal");
+        } else {
+            System.out.println("Case-Insensitive: Names are NOT equal");
+        }
+
+        sc.close();
+    }
+}
